b2bbb7f8e07a136d074ddd8dd0dfc80acdfa285e
Daniel Davies <daniel.davies@dubitlimited.com>
2020-01-03 16:23:29 +0000

    :bar_chart: Changing Article endpoint - add ID

--- app/graphql/types/article_type.rb
+++ app/graphql/types/article_type.rb
@@ -1,5 +1,6 @@
 module Types
   class ArticleType < Types::BaseObject
+    field :id, ID, null: false
     field :name, String, null: false
     field :description, String, null: true
     field :risk, String, null: false
