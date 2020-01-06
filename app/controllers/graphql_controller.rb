8dacde4320dfcf9486af99d3778252b161f833cd
Daniel Davies <daniel.davies@dubitlimited.com>
2020-01-06 17:39:27 +0000

    :art: Have GraphQL Controller inherit from ActionController::API rather than ApplicationController

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,10 +1,4 @@
-class GraphqlController < ApplicationController
-  skip_before_action :login_required
-  # If accessing from outside this domain, nullify the session
-  # This allows for outside API access while preventing CSRF attacks,
-  # but you'll have to authenticate your user separately
-  protect_from_forgery with: :null_session
-
+class GraphqlController < ActionController::API
   def execute
     variables = ensure_hash(params[:variables])
     query = params[:query]
